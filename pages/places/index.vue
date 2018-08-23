862f4975cb8f77609080406ddc92a53b71382d52
Esteban <estebandinamarca@gmail.com>
2018-08-23 11:59:26 -0300

    Initial commit

---
+++ pages/places/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="places">
+
+    <h1><nuxt-link to="/">< Inicio</nuxt-link></h1>
+
+    <article class="place">
+      <h1>Place Title</h1>
+    </article>
+
+    <article class="place">
+      <h1>Place Title 2</h1>
+    </article>
+
+  </section>
+</template>
+
+<style scoped>
+  .place {
+    background: #CCC;
+  }
+</style>
