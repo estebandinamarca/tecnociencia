95f85a5a6800bd3a02c405c851e9b63ec8a4c080
Esteban <estebandinamarca@gmail.com>
2018-09-11 23:22:16 -0300

    minimal fix in header

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,6 +22,7 @@
           <h1 class="m-0">Logo</h1>
         </div>
         <div class="w-100 d-block pl-0 pr-3 m-0 text-left">
+          <nuxt-link class="d-block font-weight-bold py-3 pl-5" to="/" @click.native.prevent="toggleNav()" exact>Inicio</nuxt-link>
           <nuxt-link class="d-block font-weight-bold py-3 pl-5" to="/rutas/arqueologia" @click.native.prevent="toggleNav()">Arqueología</nuxt-link>
           <nuxt-link class="d-block font-weight-bold py-3 pl-5" to="/rutas/astronomia" @click.native.prevent="toggleNav()">Astronomía</nuxt-link>
           <nuxt-link class="d-block font-weight-bold py-3 pl-5" to="/rutas/geologia" @click.native.prevent="toggleNav()">Geología</nuxt-link>
@@ -33,14 +34,14 @@
 
     <main>
 
-      <header class="fixed-top bg-white">
+      <header class="fixed-top d-sm-none bg-white">
         <div class="container-fluid px-0">
           <div class="row no-gutters align-items-center">
             <div class="col-3">
               <a class="h4 d-block py-3 m-0 text-center" href="" @click.prevent="toggleNav()"><i class="fas fa-bars"></i></a>
             </div>
             <div class="col-6">
-              <p class="text-center p-0 m-0">Tecnociencia</p>
+              <p class="text-center p-0 m-0"><nuxt-link class="" to="/">Tecnociencia</nuxt-link></p>
             </div>
             <div class="col-3">
               <a class="h4 d-block py-3 m-0 text-center" href="" @click.prevent="toggleNav()"><i class="fas fa-map-marker-alt"></i></a>
@@ -75,11 +76,10 @@
   }
 
   nav a {
-    border-left: 5px solid #FFF;
+    border-left: 8px solid #FFF;
   }
 
   nav a.nuxt-link-active {
-    /* background: #CCC; */
     border-color: red;
   }
 
@@ -111,7 +111,7 @@
   /* Small devices (landscape phones, 576px and up) */
   @media (max-width: 575.98px) {
     main { margin: 0; }
-    nav { width: 90%; }
+    nav { width: 85%; }
   }
 
   /* Medium devices (tablets, 768px and up) */
