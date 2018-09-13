1970e56d1684feacabbb043eb577e0de54f64530
Esteban <estebandinamarca@gmail.com>
2018-09-13 13:16:24 -0300

    vue carousel in index

--- pages/rutas/_category/_slug/index.vue
+++ pages/rutas/_category/_slug/index.vue
@@ -3,8 +3,18 @@
 
     <section class="row my-gallery">
 
-      <carousel :perPage="1" class="col-12">
-        <slide>
+      <carousel :per-page="1" class="col-12">
+        <slide v-for="(item, index) in data.items">
+          <div class="py-5 w-100 bg-primary">
+            <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
+              <a class="d-block" href="https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg" itemprop="contentUrl" data-size="1024x1024">
+                <img src="https://farm3.staticflickr.com/2567/5697107145_3c27ff3cd1_m.jpg" itemprop="thumbnail" :alt="item.fields.extracto" />
+              </a>
+              <figcaption itemprop="caption description">{{ item.fields.nombre }}</figcaption>
+            </figure>
+          </div>
+        </slide>
+        <!-- <slide>
           <div class="py-5 w-100 bg-primary">
             <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
               <a class="d-block" href="https://farm3.staticflickr.com/2567/5697107145_a4c2eaa0cd_o.jpg" itemprop="contentUrl" data-size="1024x1024">
@@ -23,7 +33,7 @@
               <figcaption itemprop="caption description">Image caption 2</figcaption>
             </figure>
           </div>
-        </slide>
+        </slide> -->
       </carousel>
 
     </section>
@@ -143,7 +153,6 @@
 
 <script type="text/javascript">
   import axios from 'axios';
-  //import { Carousel, Slide } from 'vue-carousel';
 
   export default {
     //layout: 'dark',
--- pages/rutas/_category/index.vue
+++ pages/rutas/_category/index.vue
@@ -16,6 +16,7 @@
         :href="'/rutas/' + place.fields.categoria + '/' + place.fields.slug"
       />
     </section>
+    
   </div>
 </template>
 
